5fba19b7877bb7e2c70bd3bfbcbba5342205f310
streletss <xstreletssx@gmail.com>
2019-10-30 00:44:00 +0200

    refactor stories-screenshot script

--- react/scripts/stories-screenshot/index.js
+++ react/scripts/stories-screenshot/index.js
@@ -6,19 +6,44 @@ const getModifiedTimeMsSync = require("../../../node/utils/getModifiedTimeMsSync
 
 const timestampsPath = path.resolve(__dirname, "./screenshots.timestamps.json");
 const timestampsData = JSON.parse(fs.readFileSync(timestampsPath, "utf8"));
-const timestampsDiff = {};
 const mdxFiles = findSync("./src", /\.mdx$/);
-const mdxFilesToPrint = [];
 
-function getPreviewFrameSrc() {
-  const el = document.getElementById("storybook-preview-iframe");
-  return el.src;
+populateTimestampsDiff()
+  .then(takeScreenshots)
+  .then(writeToTimestampFile)
+  .catch(err => console.log("ERROR:::", err));
+
+async function populateTimestampsDiff() {
+  return mdxFiles.reduce((diff, file) => {
+    const mtimeMs = getModifiedTimeMsSync(file);
+    if (timestampsData[file] !== mtimeMs) diff[file] = mtimeMs;
+    return diff;
+  }, {});
 }
 
-function revealStorySrcCode() {
-  [...document.querySelectorAll("button")]
-    .filter(el => el.textContent.includes("Show code"))
-    .forEach(el => el.click());
+async function takeScreenshots(diff) {
+  const mdxFilesToProcess = Object.keys(diff);
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({ width: 882, height: 1000 });
+
+  for (let index = 0; index < mdxFilesToProcess.length; index++) {
+    const file = mdxFilesToProcess[index];
+    const fileContents = fs.readFileSync(file, "utf8");
+    const pagePath = getStoryPagePath(fileContents);
+
+    if (!pagePath) continue;
+
+    await gotoStoryDocsPath(page, pagePath);
+    await page.evaluate(revealStorySrcCode);
+    await page.screenshot({
+      path: file.replace(/\.mdx$/, ".png"),
+      fullPage: true
+    });
+  }
+
+  await browser.close();
+  return diff;
 }
 
 function getStoryPagePath(storyFile) {
@@ -31,55 +56,29 @@ function getStoryPagePath(storyFile) {
     .replace(/\"/g, "");
 }
 
-function writeToTimestampFile() {
-  if (!Object.keys(timestampsDiff).length) {
+async function gotoStoryDocsPath(page, path) {
+  await page.goto(path);
+  const iframeID = "storybook-preview-iframe";
+  const getIframeSrc = id => document.getElementById(id).src;
+  const iframePath = await page.evaluate(getIframeSrc, iframeID);
+  await page.goto(iframePath);
+}
+
+function revealStorySrcCode() {
+  Array.from(document.querySelectorAll("button"))
+    .filter(el => el.textContent.includes("Show code"))
+    .forEach(el => el.click());
+}
+
+function writeToTimestampFile(diff) {
+  if (!Object.keys(diff).length) {
     console.log("Already up to date :)");
     return;
   }
 
-  const result = Object.assign(timestampsData, timestampsDiff);
+  const result = Object.assign(timestampsData, diff);
   const resultString = JSON.stringify(result, null, 2);
-  const handleError = err => {
+  fs.writeFile(timestampsPath, resultString, err => {
     if (err) throw err;
-  };
-
-  fs.writeFile(timestampsPath, resultString, handleError);
+  });
 }
-
-mdxFiles.forEach(file => {
-  const mtimeMs = getModifiedTimeMsSync(file);
-
-  if (String(timestampsData[file]) !== String(mtimeMs)) {
-    timestampsDiff[file] = String(mtimeMs);
-    mdxFilesToPrint.push(file);
-  }
-});
-
-async function gotoStoryPath
-
-async function takeScreenshots() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setViewport({ width: 882, height: 1000 });
-
-  for (let index = 0; index < mdxFilesToPrint.length; index++) {
-    const file = mdxFilesToPrint[index];
-    const fileContents = fs.readFileSync(file, "utf8");
-    const pagePath = getStoryPagePath(fileContents);
-
-    if (!pagePath) continue;
-
-
-    await page.evaluate(revealStorySrcCode);
-    await page.screenshot({
-      path: file.replace(/\.mdx$/, ".png"),
-      fullPage: true
-    });
-  }
-
-  await browser.close();
-}
-
-takeScreenshots()
-  .then(writeToTimestampFile)
-  .catch(err => console.log("ERROR:::", err));
